08a693e0a987d4a94df690869d66152aa30672d5
AnnaSadova <annasadowa@gmail.com>
2023-05-25 16:53:58 +0300

    library UI has been changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, {useState} from'react';
+import React, { useState } from "react";
 //Import Styles
-import "./styles/app.scss"
+import "./styles/app.scss";
 //Adding components
-import Player from './components/Player';
-import Song from './components/Song';
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
 //inport Util
-import data from './util';
+import data from "./util";
 
 function App() {
   //State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
-  
+
   return (
     <div className="App">
-      <Song currentSong={currentSong}/>
-      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} />
     </div>
   );
 }
